bb2358c20cf6001536a5d9b7e3d463cb638d6ad1
rub253 <rub253@gmail.com>
2025-05-25 16:34:22 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -24,3 +24,18 @@ self.addEventListener("fetch", function(event) {
             })
     );
 });
+self.addEventListener("activate", function(event) {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        console.log("🗑 Eliminando caché antigua:", cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
